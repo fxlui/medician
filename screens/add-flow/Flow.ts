cc8030146544a750f99655be49e4721ca6e21f10
James Wong <james.wong17@protonmail.com>
2021-09-06 16:53:26 +1000

    changed order of symptomtwo flow

--- screens/add-flow/Flow.ts
+++ screens/add-flow/Flow.ts
@@ -2,7 +2,7 @@
 const defaultFlow = ["severity", "occurence", "observation", "photo"];
 
 const flowOne = ["area", "severity", "defaultFlow"];
-const flowTwo = ["area", "able temperature", "temperature", "defaultFlow"];
+const flowTwo = ["able temperature", "temperature", "area", "defaultFlow"];
 const flowThree = ["which toilet", "hurt", "color", "defaultFlow"];
 const flowFour = ["what spinning", "defaultFlow"];
 const flowFive = ["sleep", "defaultFlow"];
