ddb200acabc92f9530931a7b6e99ad80c818025b
Eric Liu <me@fxlui.com>
2021-09-06 18:41:34 +0800

    WIP

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import AppLoading from "expo-app-loading";
 import * as SecureStore from "expo-secure-store";
 import * as LocalAuthentication from "expo-local-authentication";
+import * as SplashScreen from "expo-splash-screen";
 import * as ScreenOrientation from "expo-screen-orientation";
 import React, { useState, useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -17,10 +18,17 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation/Navigation";
 import { initDatabase } from "./database/dbAPI";
 
+import { Text, View } from "./components/Themed";
+import { FontAwesome } from "@expo/vector-icons";
+import TileBase from "./components/TileBase";
+
 export default function App() {
   const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined);
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const textColor = colorScheme === "light" ? "#333333" : "#fff";
+  const tileColor = colorScheme === "light" ? "#fff" : "#252525";
+
   const [lock, setLock] = useState(false);
 
   useEffect(() => {
@@ -52,8 +60,43 @@ export default function App() {
     lockOrientation();
   }, []);
 
-  if (!isLoadingComplete || !rootStore || lock) {
+  if (!isLoadingComplete || !rootStore) {
     return <AppLoading />;
+  } else if (lock) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <FontAwesome name="lock" size={40} color={textColor} />
+        <Text style={{ marginTop: 30, fontSize: 18, fontWeight: "500" }}>
+          To access Medician,
+        </Text>
+        <Text style={{ marginTop: 5, fontSize: 18, fontWeight: "500" }}>
+          you will need to unlock first.
+        </Text>
+        <TileBase
+          gradient={[tileColor, tileColor]}
+          style={{
+            marginTop: 40,
+            width: 150,
+            height: 80,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onClick={() => {
+            const tryUnlock = async () => {
+              const bio = await LocalAuthentication.authenticateAsync({
+                promptMessage: `Unlock Medician`,
+              });
+              if (bio.success) {
+                setLock(false);
+              }
+            };
+            tryUnlock();
+          }}
+        >
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>Unlock</Text>
+        </TileBase>
+      </View>
+    );
   } else {
     return (
       <RootStoreProvider value={rootStore}>
