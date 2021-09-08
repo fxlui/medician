e46d7b907b1ab132340b52c37caf40ac97170abc
Eric Liu <me@fxlui.com>
2021-09-08 16:30:20 +0800

    UI updates

--- screens/SymptomOverview.tsx
+++ screens/SymptomOverview.tsx
@@ -56,7 +56,6 @@ interface BaseData {
   item: SimpleRecordSnapshot;
 }
 
-
 const AREA_DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
@@ -78,201 +77,198 @@ const AREA_DATA = [
 
 const symptomArr = uniqueSymptoms;
 
-const SymptomOverview: React.FC<ScreenProps> = observer(
+const SymptomOverview: React.FC<ScreenProps> = observer(({ navigation }) => {
+  const colorScheme = useColorScheme();
+  const [symptomSelected, setSymptomSelected] = useState(0);
 
-  ({ navigation }) => {
+  const topRef = React.createRef<Carousel<{ title: string; type: string }>>();
+  const topBackground = colorScheme === "light" ? "white" : "#121212";
+  const [displaySymptoms, setDisplaySymptoms] = useState<SymptomItem[]>([]);
+  const { overviewStore } = useStores();
+  const routineType = (dbType: number) =>
+    dbType === 0 ? HomeTileTypes.Medication : HomeTileTypes.Exercise;
+  const areaTileEmoji = (area: string) =>
+    area === "Head"
+      ? "🤯"
+      : area === "Body"
+      ? "👕"
+      : area === "Arms"
+      ? "💪"
+      : area === "Legs"
+      ? "🦵"
+      : "";
 
-    const colorScheme = useColorScheme();
-    const [symptomSelected, setSymptomSelected] = useState(0);
-  
-    const topRef = React.createRef<Carousel<{ title: string; type: string }>>();
-    const topBackground = colorScheme === "light" ? "white" : "#121212";
-    const [displaySymptoms, setDisplaySymptoms] = useState<SymptomItem[]>([]);
-    const { overviewStore } = useStores();
-    const routineType = (dbType: number) =>
-      dbType === 0 ? HomeTileTypes.Medication : HomeTileTypes.Exercise;
-    const areaTileEmoji = (area: string) =>
-      area === "Head" ? "🤯" :
-      area === "Body" ? "👕" :
-      area === "Arms" ? "💪" :
-      area === "Legs" ? "🦵" : ""
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", async () => {
+      await overviewStore.fetchAllCollectionsAsync();
+      const fetchedCollections = symptomArr.filter((item) =>
+        overviewStore.getCollectionTypesSnapshot().includes(item.type)
+      );
+      setDisplaySymptoms(fetchedCollections);
+      if (fetchedCollections.length === 0) return;
+      await overviewStore.fetchCollectionDataAsync(
+        fetchedCollections[symptomSelected].type
+      );
+    });
+    return unsubscribe;
+  }, []);
 
-    useEffect(() => {
-      const unsubscribe = navigation.addListener("focus",
-        async () => {
-          await overviewStore.fetchAllCollectionsAsync();
-          const fetchedCollections = symptomArr.filter(
-            item =>
-              overviewStore
-              .getCollectionTypesSnapshot()
-              .includes(item.type)
-          )
-          setDisplaySymptoms(fetchedCollections);
-          if (fetchedCollections.length === 0) return;
-          await overviewStore.fetchCollectionDataAsync(fetchedCollections[symptomSelected].type);
-        }
-      );
-      return unsubscribe;
-    }, []);
-  
-    const renderSymptomTile = ({ item, index }: symptomTileProps) => {
-      return (
-        <OverviewSymptomTile
-          title={item.title}
-          iconName={item.type}
-          updater={() => {
-            topRef.current?.snapToItem(index);
-          }}
-          selected={symptomSelected === index}
-        />
-      );
-    };
-  
-    const renderAreaTile = ({ item, index }: BaseData) => {
-      return (
-        <TopTile
-          emoji={areaTileEmoji(item.area)}
-          title={item.subArea}
-          index={index}
-          selected={false}
-          updater={() =>{}}
-        />
-      );
-    };
-
-    const renderRoutineTile = ({ item, index }: routineTileProps) => {
-      return (
-        <HomeTile
-          title={item.title}
-          secondTitle={
-            routineType(item.type) === HomeTileTypes.Medication
-              ? getMedicationDoseText(item.notes)
-              : item.notes
-          }
-          subTitle={getDateText(new Date(item.time))}
-          style={{
-            marginRight: 15,
-          }}
-          index={index}
-          type={routineType(item.type)}
-          onPress={() => {
-            navigation.push("Notification", {
-              id: item.id.toString(),
-              name: item.title,
-              notes: item.notes,
-              type: routineType(item.type),
-            });
-          }}
-        />
-      );
-    };
-
-    const renderAppointmentTile = ({ item, index }: appointmentTileProps) => {
-      return (
-        <HomeTile
-          title={item.doctor}
-          subTitle={item.time.toString()}
-          style={{
-            marginRight: 15,
-          }}
-          index={index}
-          type={HomeTileTypes.Appointment}
-          onPress={() => {
-            navigation.push("Notification", {
-              id: item.id.toString(),
-              name: item.doctor,
-              notes: item.doctor,
-              type: HomeTileTypes.Appointment,
-            });
-          }}
-        />
-      );
-    };
-  
+  const renderSymptomTile = ({ item, index }: symptomTileProps) => {
     return (
-      <SafeView disableTop disableBottom style={styles.container}>
-        <View style={[styles.header, { backgroundColor: topBackground }]}>
-          <Carousel
-            data={displaySymptoms}
-            renderItem={renderSymptomTile}
-            vertical={false}
-            sliderWidth={Dimensions.get("window").width}
-            containerCustomStyle={{
-              overflow: "visible",
-            }}
-            contentContainerCustomStyle={{
-              justifyContent: "center",
-              alignItems: "flex-start",
-              overflow: "visible",
-            }}
-            itemWidth={150}
-            inactiveSlideOpacity={0.8}
-            onScrollIndexChanged={async (index) => {
-              setSymptomSelected(index);
-              await overviewStore.fetchCollectionDataAsync(displaySymptoms[index].type);
-            }}
-            ref={topRef}
-          />
-        </View>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.overflowView}>
-            <View style={{ paddingLeft: 25 }}>
-              <Text style={styles.name}>Timeline</Text>
-              <Carousel
-                style={{ overflow: "visible" }}
-                data={overviewStore.getCurrentRecordsSnapshot()}
-                renderItem={renderAreaTile}
-                inactiveSlideScale={1}
-                vertical={false}
-                sliderWidth={Dimensions.get("window").width}
-                activeSlideAlignment={"start"}
-                containerCustomStyle={{
-                  overflow: "visible",
-                }}
-                itemWidth={165}
-                inactiveSlideOpacity={1}
-                onScrollIndexChanged={() => {
-                }}
-              />
-              <Text style={styles.name}>Routines</Text>
-              <Carousel
-                data={overviewStore.getCurrentRoutinesSnapshot()}
-                renderItem={renderRoutineTile}
-                inactiveSlideScale={1}
-                vertical={false}
-                sliderWidth={Dimensions.get("window").width}
-                activeSlideAlignment={"start"}
-                containerCustomStyle={{
-                  overflow: "visible",
-                }}
-                itemWidth={165}
-                inactiveSlideOpacity={1}
-                onScrollIndexChanged={() => {
-                }}
-              />
-              <Text style={styles.name}>Appointments</Text>
-              <Carousel
-                data={overviewStore.getCurrentAppointmentsSnapshot()}
-                renderItem={renderAppointmentTile}
-                inactiveSlideScale={1}
-                vertical={false}
-                sliderWidth={Dimensions.get("window").width}
-                activeSlideAlignment={"start"}
-                containerCustomStyle={{
-                  overflow: "visible",
-                }}
-                itemWidth={165}
-                inactiveSlideOpacity={1}
-                onScrollIndexChanged={() => {
-                }}
-              />
-            </View>
-          </View>
-        </ScrollView>
-      </SafeView>
+      <OverviewSymptomTile
+        title={item.title}
+        iconName={item.type}
+        updater={() => {
+          topRef.current?.snapToItem(index);
+        }}
+        selected={symptomSelected === index}
+      />
     );
-  }
-);
+  };
+
+  const renderAreaTile = ({ item, index }: BaseData) => {
+    return (
+      <TopTile
+        emoji={areaTileEmoji(item.area)}
+        title={item.subArea}
+        index={index}
+        selected={false}
+        updater={() => {}}
+      />
+    );
+  };
+
+  const renderRoutineTile = ({ item, index }: routineTileProps) => {
+    return (
+      <HomeTile
+        title={item.title}
+        secondTitle={
+          routineType(item.type) === HomeTileTypes.Medication
+            ? getMedicationDoseText(item.notes)
+            : item.notes
+        }
+        subTitle={getDateText(new Date(item.time))}
+        style={{
+          marginRight: 15,
+        }}
+        index={index}
+        type={routineType(item.type)}
+        onPress={() => {
+          navigation.push("Notification", {
+            id: item.id.toString(),
+            name: item.title,
+            notes: item.notes,
+            type: routineType(item.type),
+          });
+        }}
+      />
+    );
+  };
+
+  const renderAppointmentTile = ({ item, index }: appointmentTileProps) => {
+    return (
+      <HomeTile
+        title={item.doctor}
+        subTitle={getDateText(new Date(item.time))}
+        style={{
+          marginRight: 15,
+        }}
+        index={index}
+        type={HomeTileTypes.Appointment}
+        onPress={() => {
+          navigation.push("Notification", {
+            id: item.id.toString(),
+            name: item.doctor,
+            notes: item.doctor,
+            type: HomeTileTypes.Appointment,
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <SafeView disableTop disableBottom style={styles.container}>
+      <View style={[styles.header, { backgroundColor: topBackground }]}>
+        <Carousel
+          data={displaySymptoms}
+          renderItem={renderSymptomTile}
+          vertical={false}
+          sliderWidth={Dimensions.get("window").width}
+          containerCustomStyle={{
+            overflow: "visible",
+          }}
+          contentContainerCustomStyle={{
+            justifyContent: "center",
+            alignItems: "flex-start",
+            overflow: "visible",
+          }}
+          itemWidth={150}
+          inactiveSlideOpacity={0.8}
+          onScrollIndexChanged={async (index) => {
+            setSymptomSelected(index);
+            await overviewStore.fetchCollectionDataAsync(
+              displaySymptoms[index].type
+            );
+          }}
+          ref={topRef}
+        />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.overflowView}>
+          <View style={{ paddingLeft: 25 }}>
+            <Text style={styles.name}>Timeline</Text>
+            <Carousel
+              style={{ overflow: "visible" }}
+              data={overviewStore.getCurrentRecordsSnapshot()}
+              renderItem={renderAreaTile}
+              inactiveSlideScale={1}
+              vertical={false}
+              sliderWidth={Dimensions.get("window").width}
+              activeSlideAlignment={"start"}
+              containerCustomStyle={{
+                overflow: "visible",
+              }}
+              itemWidth={165}
+              inactiveSlideOpacity={1}
+              onScrollIndexChanged={() => {}}
+            />
+            <Text style={styles.name}>Routines</Text>
+            <Carousel
+              data={overviewStore.getCurrentRoutinesSnapshot()}
+              renderItem={renderRoutineTile}
+              inactiveSlideScale={1}
+              vertical={false}
+              sliderWidth={Dimensions.get("window").width}
+              activeSlideAlignment={"start"}
+              containerCustomStyle={{
+                overflow: "visible",
+              }}
+              itemWidth={165}
+              inactiveSlideOpacity={1}
+              onScrollIndexChanged={() => {}}
+            />
+            <Text style={styles.name}>Appointments</Text>
+            <Carousel
+              data={overviewStore.getCurrentAppointmentsSnapshot()}
+              renderItem={renderAppointmentTile}
+              inactiveSlideScale={1}
+              vertical={false}
+              sliderWidth={Dimensions.get("window").width}
+              activeSlideAlignment={"start"}
+              containerCustomStyle={{
+                overflow: "visible",
+              }}
+              itemWidth={165}
+              inactiveSlideOpacity={1}
+              onScrollIndexChanged={() => {}}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeView>
+  );
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -283,7 +279,8 @@ const styles = StyleSheet.create({
     paddingBottom: 155,
   },
   header: {
-    paddingVertical: 30,
+    paddingBottom: 30,
+    paddingTop: 10,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 8 },
     shadowRadius: 8,
@@ -306,4 +303,3 @@ export default SymptomOverview;
 function routineType(type: number) {
   throw new Error("Function not implemented.");
 }
-
