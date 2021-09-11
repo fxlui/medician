7a1f7b9907d436db1a5aa60bf353bd2bc6c38531
Eric Liu <me@fxlui.com>
2021-09-11 20:27:22 +0800

    Fixes

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation/Navigation";
 
 import { Text, View } from "./components/Themed";
-import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import TileBase from "./components/TileBase";
 
 import { themeTextColor, themeTileColor } from "./constants/Colors";
@@ -77,7 +77,7 @@ export default function App() {
   } else if (lock) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <FontAwesome name="lock" size={40} color={textColor} />
+        <Ionicons name="ios-lock-closed" size={40} color={textColor} />
         <Text style={{ marginTop: 30, fontSize: 18, fontWeight: "500" }}>
           To access Medician,
         </Text>
--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,8 +1,47 @@
-import { ColorSchemeName, useColorScheme as _useColorScheme } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Appearance,
+  ColorSchemeName,
+  useColorScheme as _useColorScheme,
+} from "react-native";
 
 // The useColorScheme value is always either light or dark, but the built-in
 // type suggests that it can be null. This will not happen in practice, so this
 // makes it a bit easier to work with.
-export default function useColorScheme(): NonNullable<ColorSchemeName> {
-  return _useColorScheme() as NonNullable<ColorSchemeName>;
+
+interface Props {
+  colorScheme: ColorSchemeName;
+}
+
+export default function useColorScheme(
+  delay = 250
+): NonNullable<ColorSchemeName> {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  let timeout = useRef<NodeJS.Timeout | null>(null).current;
+
+  useEffect(() => {
+    Appearance.addChangeListener(onColorSchemeChange);
+
+    return () => {
+      resetCurrentTimeout();
+      Appearance.removeChangeListener(onColorSchemeChange);
+    };
+  }, []);
+
+  function onColorSchemeChange(preferences: Appearance.AppearancePreferences) {
+    resetCurrentTimeout();
+
+    timeout = setTimeout(() => {
+      setColorScheme(preferences.colorScheme);
+    }, delay);
+  }
+
+  function resetCurrentTimeout() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  }
+
+  return colorScheme as NonNullable<ColorSchemeName>;
 }
